19fd3fa81b9074d5ae08a1b3ad245afb9a80be42
Hippolyte <hi.soulier@gmail.com>
2023-11-16 11:11:13 +0100

    GH actions

--- .github/actions/clean-cache.js
+++ .github/actions/clean-cache.js
@@ -1,11 +1,16 @@
 module.exports = async ({ github, context }) => {
-  console.log("About to clear")
+  console.log(
+    "About to clear",
+    `${context?.payload?.owner || "no values"}:${
+      context?.payload?.ref || "no values"
+    }`
+  )
   // console.dir(context, { depth: null })
 
   const { data: pull } = await github.rest.pulls.list({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    ref: `${context.payload.owner.name}:${context.payload.ref}`,
+    ref: `${context.payload.owner}:${context.payload.ref}`,
     state: "open",
   })
   console.log("List of PR")
